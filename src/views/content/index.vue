5d33edb628369dce083bbb226fd82275fa8028da
wangruilong <862452159@qq.com>
2017-04-07 18:14:22 +0800

    规范目录结构

---
+++ src/views/content/index.vue
@@ -0,0 +1,21 @@
+<style scoped>
+  body{
+    background: gray;
+  }
+</style>
+<template>
+  <div>
+  </div>
+</template>
+<script>
+  export default{
+    data(){
+      return{
+
+      }
+    },
+    components:{
+      'date':require('components/date.vue')
+    }
+  }
+</script>
