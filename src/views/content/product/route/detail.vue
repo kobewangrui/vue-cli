719542a3dbbd56eff9bd644a4e5df4444af87943
wangruilong <862452159@qq.com>
2017-04-08 12:45:25 +0800

    修改轮播图焦点样式

--- src/views/content/product/route/detail.vue
+++ src/views/content/product/route/detail.vue
@@ -18,10 +18,10 @@
                             </div>
                         </div>
                         <!-- 如果需要分页器 -->
-                        <!--<div class="swiper-pagination"></div>-->
+                        <div class="swiper-pagination swiper-pagination-white"></div>
                         <!-- 如果需要导航按钮 -->
-                        <!--<div class="swiper-button-prev"></div>
-                        <div class="swiper-button-next"></div>-->
+                        <div class="swiper-button-prev swiper-button-white"></div>
+                        <div class="swiper-button-next swiper-button-white"></div>
                         <!--<span class="after-group">跟团游</span>-->
                         <!--<span class="ground">地接</span>-->
                         <!--<span class="self">自助</span>-->
@@ -29,15 +29,15 @@
                         <!--<span class="liner">邮轮</span>-->
                         <!--<span class="drive-self">自驾</span>-->
                     </div>
-                    <div class="calendar">
-                    <p class="headDate">
-                        <span class="prevMonth" @click="prevMonth"> < </span>
-                        <span>{{curYear}}年{{curMonth}}月</span>
-                        <span class="nextMonth" @click="nextMonth"> > </span>
-                    </p>
-                    <ul class="week">
-                        <li v-for="i in week">{{i}}</li>
-                    </ul>
+                    <!--<div class="calendar">
+                        <p class="headDate">
+                            <span class="prevMonth" @click="prevMonth"> < </span>
+                            <span>{{curYear}}年{{curMonth}}月</span>
+                            <span class="nextMonth" @click="nextMonth"> > </span>
+                        </p>
+                        <ul class="week">
+                            <li v-for="i in week">{{i}}</li>
+                        </ul>
                         <table class="table">
                             <tr v-for="item in days">
                                 <td v-for="key in item" :class="{'active':new Date().getDate() == key}" :id="curYear+'-'+curMonth+'-'+key" @click="activeDate(curYear+'-'+curMonth+'-'+key)">
@@ -56,42 +56,37 @@
                                     <span class="price" v-if="period">
                                         ￥{{period.price.adult.inner}}
                                     </span>
-                                    
-                                    
-                                    <!--如果是特价添加下下面的两个span-->
-                                    <!--<span class="shape"></span>
-                                    <span class="specialText">特</span>-->
                                     <img :src="require('assets/image/specialSign.png')" alt="">
                                 </td>
                             </tr>
                         </table>
-                    </div>
+                    </div>-->
                 </div>               
                 <div class="detailIntro">
                     <div class="introTitle">
-                        <span class="titleName">{{route.title}}</span>
-                        <span class="spicial" v-if="route.isSpecial=='Y'">特价</span>
-                        <span class="rightTime" v-if="route.immediatelyPay=='on'">即时</span>
-                        <span class="TAC" v-if="route.routeType=='TAC'">TAC</span>
+                        <span class="titleName">这是标这是标这是标这是标这是标这是标这是标这是标题</span>
+                        <span class="spicial">特价</span>
+                        <!--<span class="rightTime">即时</span>
+                        <span class="TAC">TAC</span>-->
                     </div>
                     <div class="detailList"> 
                         <p>
-                            产品编号：<span class="listContent">{{route.productId}}</span>
+                            产品编号：<span class="listContent">cp100000</span>
                         </p>
                         <p>
-                            行程天数：<span class="listContent listDays">{{route.days}}天</span>
-                            提前报名天数：<span class="listContent">{{route.lead}}天</span>
+                            行程天数：<span class="listContent listDays">12天</span>
+                            提前报名天数：<span class="listContent">15天</span>
                         </p>
                         <p>
-                            即时交易：<span class="listContent" v-if="route.immediatelyPay=='on'">是</span><span class="listContent" v-if="route.immediatelyPay!='on'">否</span>
+                            即时交易：<span class="listContent">是</span>
                             <span class="listQuestion">?</span>
                             <span>即时交易是指即时确认，否则需等待卖家却后才可付款。</span>
                         </p>
                         <p>
-                            往返交通：<span class="listContent">未绑定</span>
+                            往返交通：<span class="listContent">飞机</span>
                         </p>
                         <p>
-                            出发地/目的地：<span class="listContent">{{route.departure}}--{{route.destination}}</span>
+                            出发地/目的地：<span class="listContent">陕西西安--内蒙古呼和浩特</span>
                         </p>
                     </div>
                     <div class="detailPrice">
@@ -107,10 +102,10 @@
                                 <span class="priceTitle">结算价：</span>
                                 <!--<span class="priceTitle">特价：</span>-->
                                 <span class="priceSymbol">￥</span>
-                                <span class="settlementPrice">{{selectedPeriod.price.adult.inner}}</span>/成人
+                                <span class="settlementPrice">123456</span>/成人
                                 <span class="postionChildren">
                                     <span class="priceSymbol symbolChildren">￥</span>
-                                    <span class="settlementPrice">{{selectedPeriod.price.child.inner}}</span>/儿童
+                                    <span class="settlementPrice">1234</span>/儿童
                                 </span>
                             </span>
                         </p>
@@ -118,16 +113,16 @@
                             <span>
                                 <span class="priceTitle">建议零售：</span>
                                 <span class="priceSymbol">￥</span>
-                                <span class="retailPrice">{{selectedPeriod.price.adult.outer}}</span>/成人
+                                <span class="retailPrice">1234</span>/成人
                                 <span class="postionChildren">
                                     <span class="priceSymbol symbolChildren">￥</span>
-                                    <span class="retailPrice">{{selectedPeriod.price.child.outer}}</span>/儿童
+                                    <span class="retailPrice">1234</span>/儿童
                                 </span>
                             </span>
                         </p>
                         <p class="buyNow">
                             <!--<a href="javscript:void(0);" class="noJurisdiction">暂无权限</a>-->
-                            <router-link :to="{path:'/mall/submitOrderDomestic',query:{productId:route.productId,date:selectedPeriod.date}}" class="buyRoute">立即预定</router-link>
+                            <router-link :to="{path:'/mall/submitOrderDomestic',query:{productId:route.productId,date:'2016-05-04'}}" class="buyRoute">立即预定</router-link>
                             <button class="joinTravell">加入收客</button>
                             <span class="dealIntro">交易说明</span>
                         </p>
@@ -141,14 +136,12 @@
                                 </div>
                             </span>
                             <span>
-                                <template v-if="colllection==2">
-                                    <img :src="require('assets/image/collection.png')">
-                                    <span>收藏线路</span>
-                                </template>
-                                <template v-else>
+                                <img :src="require('assets/image/collection.png')">
+                                <span>收藏线路</span>
+                                <!--<template v-else>
                                     <img :src="require('assets/image/collectioned.png')">
                                     <span>已收藏</span>
-                                </template>
+                                </template>-->
                             </span>
                             <span>
                                 <img :src="require('assets/image/download.png')">
@@ -160,9 +153,9 @@
             </div>
         </div>
         <div class="brandIntro">
-            <img :src="route.Org_Logo"  class="brandImg">
+            <img :src="require('assets/image/1.jpg')"  class="brandImg">
             <div class="brandName">
-                <p class="ellipsis">{{route.Org_OrganizationName}}</p>
+                <p class="ellipsis">众信假期</p>
                 <p class="ellipsis">西安某哦某旅行有限公司西安某哦某旅行有限公司西安某哦某旅行有限公司西安某哦某旅行有限公司西安某哦某旅行有限公司西安某哦某旅行有限公司西安某哦某旅行有限公司</p>
             </div>
             <div class="brandConnect">
@@ -207,54 +200,51 @@
                 <a href="#remark"><li>备注</li></a>
                 <a href="#options"><li>附件</li></a>
                 <li class="tableConnect" @click="popChange">联系商家</li>
-                <router-link :to="{path:'/mall/confirmOrder',query:{productId:route.productId}}" class="tableReserve" tag="li">立即预定</router-link>
+                <router-link :to="{path:'/mall/confirmOrder',query:{productId:'cp10000'}}" class="tableReserve" tag="li">立即预定</router-link>
                 <!--<li class="tableReserve">暂无权限</li>-->
             </ul>
             <div class="routeFeature">
                 <p class="routeDetailtitle" id="routeFeature">线路特色</p>
-                <div  class="discription" v-html="route.feature"></div>
+                <div  class="discription">这里是直接输出html代码这里是直接输出html代码这里是直接输出html代码这里是直接输出html代码这里是直接输出html代码</div>
             </div>
             <div class="tripContent">
                 <p class="routeDetailtitle" id="tripContent">行程内容</p>
-                {{route.days}}
+                这里是直接输出html代码这里是直接输出html代码这里是直接输出html代码这里是直接输出html代码这里是直接输出html代码这里是直接输出html代码
                 <div class="daysNavOuter">
-                    <a v-for="day in route.days" :href="'#days'+day" class="daysNav" :class="'navDays'+day">第{{day}}天
+                    <!--<a v-for="day in 7" :href="'#days'+day" class="daysNav" :class="'navDays'+day">第{{day}}天-->
                     </a>
                 </div>
   
-                <div class="days" v-for="atom in route.content" :id="'days' + atom.day">
-                    <div class="daysNumber">第{{atom.day}}天</div>
+                <div class="days" v-for="atom in 3" :id="'days' + atom.day">
+                    <div class="daysNumber">第{{atom}}天</div>
                     <div class="daysDetail">
                         <p class="daysDetails">
-                            <span>{{atom.srcDst}}</span>
-                            <span>交通：{{atom.subTraffic}}</span>
+                            <span>西安一日游</span>
+                            <span>交通：飞机</span>
                         </p>
                         <p class="daysDetails">
-                            <span>用餐：{{atom.subDining}}</span>
-                            <span>住宿：{{atom.subAccommodation}}</span>
+                            <span>用餐：泡馍</span>
+                            <span>住宿：大酒店</span>
                         </p>
-                        {{subPic=atom.subPic?atom.subPic.split(',') : [] | hide}}
-                        {{subPic.pop()}}
                         <div class="daysImg">
-                            <img v-for="pic in subPic" :src="pic"> 
+                            <img v-for="i in 3" :src="require('assets/image/3.jpg')"> 
                         </div>
-                        <p class="discription" v-html="atom.content"></p>
+                        <p class="discription">这里可以书出html代码这里可以书出html代码这里可以书出html代码这里可以书出html代码这里可以书出html代码</p>
                     </div>
                 </div>
             </div>
-            <div class="costDetail" v-for="item in listIntro" v-if="item.PRODUCT_ID==productId">
+            <div class="costDetail">
                 <p class="routeDetailtitle" id="costDetail">费用明细</p>
-                <div class="discription" v-html="route.costDetail"></div>
+                <div class="discription">这里可以书出html代码这里可以书出html代码这里可以书出html代码这里可以书出html代码这里可以书出html代码</div>
             </div>
-            <div class="remark" v-for="item in listIntro" v-if="item.PRODUCT_ID==productId">
+            <div class="remark">
                 <p class="routeDetailtitle" id="remark">备注</p>
-                <div class="discription" v-html="route.reserveNotes"></div>
+                <div class="discription">这里可以书出html代码这里可以书出html代码这里可以书出html代码这里可以书出html代码这里可以书出html代码</div>
             </div>
             <div class="options">
                 <p class="routeDetailtitle" id="options">附件</p>
                 <span class="optionsIntro">点击文件直接下载</span>
-                未绑定
-                <!--<p v-for="(item,$index) in downList"><a :href="downUrlList[$index]" class="optionsDetail">{{item}}</a></p>-->
+                <p v-for="i in 3"><a class="optionsDetail">手机打开了房间昆仑山.doc</a></p>
             </div>
         </div>
     </div>
