27a020a930a01b9b0d37f6d477b2b5cd25915c09
wangruilong <862452159@qq.com>
2017-04-09 22:36:07 +0800

    将价格分为正常价和特价

--- src/components/lineGroupL.vue
+++ src/components/lineGroupL.vue
@@ -10,20 +10,16 @@
         </div>
         <div class="product-show">
              <router-link tag="div" :to="{path:'/product/route/detail',query:{productId:'cp10000'}}" v-for="(item,$index) in 12" :class="{'first':$index == 0}" :key="item.id">
-                    <template v-if="$index%2==0">
-                        <span class="TAC">TAC</span>
-                    </template>
-                    <template v-else>
-                        <span class="spicial">特价</span>
-                    </template>
+                <span class="spicial" v-if="$index%4==0 && $index!=0">特价</span>
+                <span v-else></span>
                 <img :src="require('assets/image/1.jpg')" alt="线路展示">
                 <p class="ellipsis">昆达利撒发射点发射点发生十分士大夫</p>
                 <p>
-                <!--<span class="original-price">原价：￥1231</span>-->
-                    <template v-if="$index!=0">
+                <span class="original-price" v-if="$index%4==0 && $index!=0">原价：￥1231</span>
+                    <template v-if="$index!=0 && $index%4!=0">
                         <span>余量：8</span>
                     </template>
-                    <template v-else>
+                    <template v-else-if="$index==0 && $index%4==0">
                         <span>陕中旅雁塔路分公司</span>
                     </template>
                     <span>￥12313</span>
