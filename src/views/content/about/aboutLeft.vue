3e62a0ab163b9591368691e17453556024c92f65
wangruilong <862452159@qq.com>
2017-04-09 15:20:20 +0800

    调整路由为exact模式

--- src/views/content/about/aboutLeft.vue
+++ src/views/content/about/aboutLeft.vue
@@ -2,11 +2,11 @@
 <template>
     <div class="content-left">
         <p>关于</p>
-	    <router-link to="/about">关于我们</router-link>
-        <router-link to="/about/contact">联系我们</router-link>
-        <router-link to="/about/add">加入我们</router-link>
+	    <router-link to="/about" exact>关于我们</router-link>
+        <router-link to="/about/contact" exact>联系我们</router-link>
+        <router-link to="/about/add" exact>加入我们</router-link>
         <p>合作</p>
-        <router-link to="/about/cooperation">广告合作</router-link>
-        <router-link to="/about/popularize">推广合作</router-link>
+        <router-link to="/about/cooperation" exact>广告合作</router-link>
+        <router-link to="/about/popularize" exact>推广合作</router-link>
     </div>
 </template>
