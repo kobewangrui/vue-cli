a492dce336a99470b325dc2c281d0e1f34eee032
wangruilong <862452159@qq.com>
2017-04-09 22:21:19 +0800

    修改链接跳转以及传参

--- src/components/lineGroupR.vue
+++ src/components/lineGroupR.vue
@@ -4,7 +4,7 @@
         <p class="line-right-title">{{categroy[routeCategroy].title}}</p>
         <ul>
             <template v-for="(item,$index) in 10">
-                <li v-if="$index<3">
+                <router-link :to="{path:'/product',query:{routeCategroy:url}}" tag="li" v-if="$index<3">
                     <div class="hot-line-img">
                         <img :src="require('assets/image/2.jpg')">
                         <span class="lineHotNumber">{{$index + 1}}</span>
@@ -17,15 +17,14 @@
                             <span>￥1000</span>
                         </p>
                     </div>
-                </li>
-                
-                <li v-else class="hot-after">
+                </router-link>
+                <router-link v-else class="hot-after" :to="{path:'/product',query:{routeCategroy:url}}" tag="li">
                     <span>{{$index + 1}}</span>
                     <span class="ellipsis">喀什酱豆腐立刻就是打开垃圾分厘卡时间</span>
-                </li>
+                </router-link>
             </template>
         </ul>
-        <p>查看更多 ></p>
+        <router-link :to="{path:'/product',query:{routeCategroy:url}}" tag="p">查看更多 ></router-link>
     </div>
 </template>
 <script>    
@@ -34,7 +33,8 @@ export default {
         "routeCategroy"
     ],
     data(){
-        return{
+        return{            
+            url:undefined,
             categroy:{
                 NEARBY:{
                     title:'周边热销',
@@ -52,6 +52,19 @@ export default {
         // $.post('/rest/product/route/onSale',{pageSize:5,routeCategroy:this.routeCategroy},(resp)=>{
         //     this.routes=resp.data
         // })
+    },
+    created(){
+        switch(this.routeCategroy){
+            case 'NEARBY':
+                this.url = 'NEARBY';
+                break;
+            case 'INLAND':
+                this.url = 'INLAND';
+                break;
+            case 'OVERSEAS':
+                this.url = 'OVERSEAS';
+                break;
+        }
     }
 }
 </script>
