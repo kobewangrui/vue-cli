0dee8ddb797184a10cdbca3ff8d81b58c648dc97
wangruilong <862452159@qq.com>
2017-04-09 16:12:05 +0800

    解决新版vue v-for语法警告

---
+++ src/views/content/help/usuallyQuestion.vue
@@ -0,0 +1,29 @@
+<style lang="css" src="assets/css/usuallyQuestion.css" scoped></style>
+<template>
+    <div class="container">
+        <help-top></help-top>
+        <div class="content-right">
+            <ul class="list">
+                <router-link :to="list.targetAdress" v-for="list in questionList" :key="list.id" tag="li"><span></span>{{list.tit}}</router-link>
+            </ul>
+        </div>
+        <ul class="connectUs">
+            <li>
+                <p></p>
+                <p>客服QQ</p>
+                <p>3102317067</p>
+            </li>
+            <li>
+                <p></p>
+                <p>客服电话</p>
+                <p>400-9900-516</p>
+            </li>
+            <li>
+                <p></p>
+                <p>客服邮箱</p>
+                <p>support@gaiatrip.com</p>
+            </li>
+        </ul>
+    </div>
+</template>
+<script src="assets/js/usuallyQuestion.js"></script>
