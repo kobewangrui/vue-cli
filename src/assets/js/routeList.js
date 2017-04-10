8c7d7e7ee1b1eb3a86f58d059948bdcbfa275343
wangruilong <862452159@qq.com>
2017-04-10 14:18:59 +0800

    调整刷新页面后保持筛选参数

--- src/assets/js/routeList.js
+++ src/assets/js/routeList.js
@@ -1,75 +1,73 @@
 export default {
     data(){
-        // const query = Object.assign({},this.$route.query);
-        // //初始化线路类型
-        // if((typeof query.groupWay)==='string'){
-        //     query.groupWay=[query.groupWay]
-        // }else{
-        //     query.groupWay=Object.assign([],this.$route.query.groupWay)
-        // }
-        // //初始化出行方式
-        // if((typeof query.traffic)==='string'){
-        //     query.traffic=[query.traffic]
-        // }else{
-        //     query.traffic=Object.assign([],this.$route.query.traffic)
-        // }
-        // // 初始化行程天数
-        // if((typeof query.days)==='string'){
-        //     query.days=[query.days]
-        // }else{
-        //     query.days=Object.assign([],this.$route.query.days)
-        // }
-        // //初始化排序
-        // if((typeof query.sort)==='string'){
-        //     query.sort=[query.sort]
-        // }else{
-        //     query.sort=Object.assign([],this.$route.query.sort)
-        // }
-        // //初始化特价
-        // if((typeof query.IS_SPECIAL)==='string'){
-        //     query.IS_SPECIAL=[query.IS_SPECIAL]
-        // }else{
-        //     query.IS_SPECIAL=Object.assign([],this.$route.query.IS_SPECIAL)
-        // }
-        // //初始化即时确认
-        // if((typeof query.IMMEDIATELY_PAY)==='string'){
-        //     query.IMMEDIATELY_PAY=[query.IMMEDIATELY_PAY]
-        // }else{
-        //     query.IMMEDIATELY_PAY=Object.assign([],this.$route.query.IMMEDIATELY_PAY)
-        // }
-        // //初始化出团日期
-        // if((typeof query.travelTimeStart)==='string'){
-        //     query.travelTimeStart=[query.travelTimeStart]
-        // }else{
-        //     query.travelTimeStart=Object.assign([],this.$route.query.travelTimeStart)
-        // }
-        // //初始化回团日期
-        // if((typeof query.travelTimeEnd)==='string'){
-        //     query.travelTimeEnd=[query.travelTimeEnd]
-        // }else{
-        //     query.travelTimeEnd=Object.assign([],this.$route.query.travelTimeEnd)
-        // }
-        // //初始化价格区间开始
-        // if((typeof query.priceStart)==='string'){
-        //     query.priceStart=[query.priceStart]
-        // }else{
-        //     query.priceStart=Object.assign([],this.$route.query.priceStart)
-        // }
-        // //初始化价格区间结束
-        // if((typeof query.priceEnd)==='string'){
-        //     query.priceEnd=[query.priceEnd];
-        // }else{
-        //     query.priceEnd=Object.assign([],this.$route.query.priceEnd)
-        // }
-        // //初始化当前页
-        // if((typeof query.pageIndex)==='string'){
-        //     query.pageIndex=[query.pageIndex];
-        //     console.log("if")
-        // }else{
-        //     console.log("else")
-        //     console.log(this.pageIndex)
-        //     query.pageIndex=Object.assign([],this.$route.query.pageIndex)
-        // }
+        const query = Object.assign({},this.$route.query);
+        //初始化线路类型
+        if((typeof query.groupWay)==='string'){
+            query.groupWay=[query.groupWay]
+        }else{
+            query.groupWay=Object.assign([],this.$route.query.groupWay)
+        }
+        //初始化出行方式
+        if((typeof query.traffic)==='string'){
+            query.traffic=[query.traffic]
+        }else{
+            query.traffic=Object.assign([],this.$route.query.traffic)
+        }
+        // 初始化行程天数
+        if((typeof query.days)==='string'){
+            query.days=[query.days]
+        }else{
+            query.days=Object.assign([],this.$route.query.days)
+        }
+        //初始化排序
+        if((typeof query.sort)==='string'){
+            query.sort=[query.sort]
+        }else{
+            query.sort=Object.assign([],this.$route.query.sort)
+        }
+        //初始化特价
+        if((typeof query.IS_SPECIAL)==='string'){
+            query.IS_SPECIAL=[query.IS_SPECIAL]
+        }else{
+            query.IS_SPECIAL=Object.assign([],this.$route.query.IS_SPECIAL)
+        }
+        //初始化即时确认
+        if((typeof query.IMMEDIATELY_PAY)==='string'){
+            query.IMMEDIATELY_PAY=[query.IMMEDIATELY_PAY]
+        }else{
+            query.IMMEDIATELY_PAY=Object.assign([],this.$route.query.IMMEDIATELY_PAY)
+        }
+        //初始化出团日期
+        if((typeof query.travelTimeStart)==='string'){
+            query.travelTimeStart=[query.travelTimeStart]
+        }else{
+            query.travelTimeStart=Object.assign([],this.$route.query.travelTimeStart)
+        }
+        //初始化回团日期
+        if((typeof query.travelTimeEnd)==='string'){
+            query.travelTimeEnd=[query.travelTimeEnd]
+        }else{
+            query.travelTimeEnd=Object.assign([],this.$route.query.travelTimeEnd)
+        }
+        //初始化价格区间开始
+        if((typeof query.priceStart)==='string'){
+            query.priceStart=[query.priceStart]
+        }else{
+            query.priceStart=Object.assign([],this.$route.query.priceStart)
+        }
+        //初始化价格区间结束
+        if((typeof query.priceEnd)==='string'){
+            query.priceEnd=[query.priceEnd];
+        }else{
+            query.priceEnd=Object.assign([],this.$route.query.priceEnd)
+        }
+        //初始化当前页
+        if((typeof query.pageIndex)==='string'){
+            query.pageIndex=[query.pageIndex];
+        }else{
+            console.log(this.pageIndex)
+            query.pageIndex=Object.assign([],this.$route.query.pageIndex)
+        }
         return{
             routes:[],
 
@@ -77,7 +75,7 @@ export default {
                 pageIndex:0,
                 pageMaxIndex:10
             },
-            query:{},
+            query:query,
             calendarShow:false,
             start:'2016-12-25',
             end:'2016-12-29',
@@ -165,16 +163,10 @@ export default {
                         arg:"NINE"
                     }
                 ]
-            },
-            list:{
-                productList:{}
-            },
-            productSign:'',
-            trafficSign:'',
-            daysSign:''
+            }
         }
     },
-    mounted(){
+    created(){
         this.routeData = this.$route.query.groupWay;
         this.priceStarts = this.$route.query.priceStart;
         this.priceEnds = this.$route.query.priceEnd;
@@ -201,11 +193,8 @@ export default {
     },
     methods:{
         fetchData:function(){
-            // console.warn(this.$route.query.routeCategroy)
             // $.post('/rest/product/route/onSale',{routeCategroy:this.$route.query.routeCategroy},(resp)=>{
             //     console.warn(resp.data)
-            //     this.routes=resp.data
-            //     this.paginate=resp.paginate
             // })
         },
         clear(item){
