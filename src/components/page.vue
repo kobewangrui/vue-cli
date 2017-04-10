648fc7663c2f117b161827f79b96d67d0297ef92
wangruilong <862452159@qq.com>
2017-04-10 18:09:49 +0800

    页码逻辑，体验不佳

--- src/components/page.vue
+++ src/components/page.vue
@@ -1,15 +1,24 @@
 <style lang="css" src="assets/css/page.css" scoped></style>
 <template>
     <div class="page">
+        <div v-show="false">
+            {{
+                $route.query.pageIndex = parseInt($route.query.pageIndex)
+            }}
+        </div>
         <div class="pageNav">{{paginate.pageIndex+1}}/{{paginate.pageMaxIndex+1}}页</div>
-        <router-link class="prevPage" :to="{path:$route.path,query:Object.assign({},$route.query,{pageIndex:paginate.pageIndex-1>0?paginate.pageIndex-1:0})}"></router-link>
+        <router-link class="prevPage" :to="{path:$route.path,query:Object.assign({},$route.query,{pageIndex:$route.query.pageIndex-1>0?$route.query.pageIndex-1:0})}"></router-link>
         <ul>
-            <router-link tag="li" :to="{path:$route.path,query:Object.assign({},$route.query,{pageIndex:paginate.pageIndex})}">{{paginate.pageIndex+1}}</router-link>
-            <!--<li v-if="pageCount<7" v-for="i in pageCount" :class="{'active':i == pageIndex}" @click="curPage(i)">{{i}}</li>
-            <li v-if="pageCount>=7" v-for="i in pageCount" :class="{'active':i == pageIndex}" @click="curPage(i)" v-show="i==4?i='......':(i==pageIndex-2?i='......':(i==1||i==2||i==3||i==pageCount-2||i==pageCount-1||i==pageCount||i==pageIndex||i==pageIndex-1||i==pageIndex+1))">{{i}}</li>
-            <li v-if="pageCount>=7" v-for="i in pageCount" :class="{'active':i == pageIndex}" @click="curPage(i)" v-show="pageIndex>4?(i==1)">{{i}}</li>        -->
+            <router-link 
+                tag="li" 
+                :key="index.id" 
+                v-for="index in paginate.pageMaxIndex+1" 
+                :to="{path:$route.path,query:Object.assign({},$route.query,{pageIndex:index-1})}"
+                v-show="$route.query.pageIndex+1==index || $route.query.pageIndex+1==index+1 || $route.query.pageIndex+1==index+2 || $route.query.pageIndex+1==index+3 || $route.query.pageIndex+1==index-1 || $route.query.pageIndex+1==index-2 || $route.query.pageIndex+1==index-3"
+                :class="{'active':$route.query.pageIndex+1 == index}"
+                >{{index}}</router-link>
         </ul>
-        <router-link class="nextPage" :to="{path:$route.path,query:Object.assign({},$route.query,{pageIndex:paginate.pageIndex+1<paginate.pageMaxIndex?paginate.pageIndex+1:paginate.pageMaxIndex})}"></router-link>        
+        <router-link class="nextPage" :to="{path:$route.path,query:Object.assign({},$route.query,{pageIndex:$route.query.pageIndex+1<paginate.pageMaxIndex?$route.query.pageIndex+1:paginate.pageMaxIndex})}"></router-link>   
     </div>
 </template>
 <script src="assets/js/page.js"></script>
