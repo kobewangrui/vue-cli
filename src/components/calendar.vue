f54c3975189090c060d2ad35dc42dbcd64220453
wangruilong <862452159@qq.com>
2017-04-10 15:26:37 +0800

    修改为双日历

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -5,29 +5,27 @@
             <p class="headDate">
                 <span class="prevMonth" @click="prevMonth"> < </span>
                 <span>{{curYear}}年{{curMonth}}月</span>
-                <!--<span class="nextMonth" @click="nextMonth"> > </span>-->
             </p>
             <ul class="week">
                 <li v-for="i in week">{{i}}</li>
             </ul>
             <table class="table">
                 <tr v-for="item in days">
-                    <td v-for="key in item" :class="{'active':new Date().getDate() == key}" v-if="" @click="propsDate(key)">{{key}}</td>
+                    <td v-for="key in item" :class="{'active':new Date().getDate() == key}" @click="propsDate(key)">{{key}}</td>
                 </tr>
             </table>
         </div>
         <div class="calendar rgihtCalendar">
             <p class="headDate">
-                <!--<span class="prevMonth" @click="prevMonth"> < </span>-->
-                <span>{{curYear}}年{{curMonth}}月</span>
+                <span>{{curYear2}}年{{curMonth2}}月</span>
                 <span class="nextMonth" @click="nextMonth"> > </span>
             </p>
             <ul class="week">
                 <li v-for="i in week">{{i}}</li>
             </ul>
             <table class="table">
-                <tr v-for="item in days">
-                    <td v-for="key in item" :class="{'active':new Date().getDate() == key}" v-if="" @click="propsDate(key)">{{key}}</td>
+                <tr v-for="item in days2">
+                    <td v-for="key in item" :class="{'active':new Date().getDate() == key}" @click="propsDate2(key)">{{key}}</td>
                 </tr>
             </table>
         </div>
