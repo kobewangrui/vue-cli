569e54b4940141b0fcdddf1aeb2adf620782a8fa
wangruilong <862452159@qq.com>
2017-04-10 18:09:26 +0800

    提交页码逻辑，存在体验不好bug

--- src/assets/js/page.js
+++ src/assets/js/page.js
@@ -5,29 +5,5 @@ export default{
     },
     props:[
         'paginate'
-    ],
-    // methods:{
-    //     curPage(item){
-    //         this.pageIndex = item;
-    //     },
-    //     prevPage(){
-    //         if(this.pageIndex <= 2){
-    //             $(".page>.prevPage").css("pointer-events","none");
-    //         }else{
-    //             this.pageIndex--;
-    //         }
-    //     },
-    //     nextPage(){
-    //         if(this.pageIndex >= this.pageCount){
-    //             $(".page>.nextPage").css("pointer-events","none");
-    //         }else{
-    //             this.pageIndex++;
-    //         }
-    //     },
-    //     currentPage(){
-    //         if(this.pageIndex===1 && this.pageCount>6){
-    //             $(".page>ul>li").eq(4).text("......").css("display","inline-block");
-    //         }
-    //     }
-    // }
+    ]
 }
